2093a8bac3166f6ad2d7073f1d7649680def1c51
le.anh.thanh <le.anh.thanh@sun-asterisk.com>
2025-03-24 14:28:59 +0700

    Test with multiple simplecov formatters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 require "simplecov"
+require "simplecov-cobertura"
 
 SimpleCov.start "rails" do
   coverage_dir ".reports/coverage"
 end
 
-require "simplecov-cobertura"
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::CoberturaFormatter
+]
 
 require "calculator"
 require "minitest/autorun"
